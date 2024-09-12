b249c98f38f0866d2864e37721ab7ec38d59919f
mosenpai <mo4vids@gmail.com>
2024-09-12 15:26:01 +0200

    complete tutorial Introduction of Avalonia

--- testprojects/GetStartedApp/ViewModels/MainWindowViewModel.cs
+++ testprojects/GetStartedApp/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,6 @@
 public partial class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
+    public string Greeting => "Welcome to Avalonia! I added this text";
 #pragma warning restore CA1822 // Mark members as static
 }
--- testprojects/GetStartedApp/Views/MainWindow.axaml.cs
+++ testprojects/GetStartedApp/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
+using System.Diagnostics;
 
 namespace GetStartedApp.Views;
 
@@ -7,5 +10,27 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void ButtonClicked(object source, RoutedEventArgs args)
+    {
+        if (Double.TryParse(celsius.Text, out double C))
+        {
+            double F = C * (9d / 5d) + 32;
+            fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            celsius.Text = "0";
+            fahrenheit.Text = "0";
+
+        }
+
+        Debug.WriteLine($"Click! Celsius={celsius.Text}");
+    }
+
+    private void TextBox_TextChanging(object? sender, Avalonia.Controls.TextChangingEventArgs e)
+    {
+        ButtonClicked(sender, e);
     }
 }
