fc942b8332858ab54b301b5c665f5f78fa82d6b7
GlennVerstraelenFontys <70883027+GlennVerstraelenFontys@users.noreply.github.com>
2024-09-12 14:31:05 +0200

    first build

---
+++ testprojects/GetStartedApp2/GetStartedApp2.Android/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content.PM;
+
+using Avalonia;
+using Avalonia.Android;
+using Avalonia.ReactiveUI;
+
+namespace GetStartedApp2.Android;
+
+[Activity(
+    Label = "GetStartedApp2.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity<App>
+{
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .WithInterFont()
+            .UseReactiveUI();
+    }
+}
---
+++ testprojects/GetStartedApp2/GetStartedApp2/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace GetStartedApp2.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
