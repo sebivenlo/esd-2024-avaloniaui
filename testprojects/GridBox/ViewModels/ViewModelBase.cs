76e95054afc36df970ea85ec507354fff5c71b5a
GlennVerstraelenFontys <70883027+GlennVerstraelenFontys@users.noreply.github.com>
2024-09-23 12:34:08 +0200

    project deletion and creation

--- testprojects/GridBox/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace GridBox.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
---
+++ testprojects/GridPersona/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GridPersona.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
