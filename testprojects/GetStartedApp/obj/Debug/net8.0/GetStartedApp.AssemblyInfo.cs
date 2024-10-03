dfd8183dcaaa4e6502d12bc4225a530a288114f6
mosenpai <mo4vids@gmail.com>
2024-10-03 14:06:56 +0200

    make character inside ViewBox so body parts stay proportionate and height and width can be mutable

--- testprojects/GetStartedApp/obj/Debug/net8.0/GetStartedApp.AssemblyInfo.cs
+++ testprojects/GetStartedApp/obj/Debug/net8.0/GetStartedApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GetStartedApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c75e583a11d00bd27fee8922949df501c84debcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acee9e8a8831c6a05e171ed43b1fbfe2bbfc1841")]
 [assembly: System.Reflection.AssemblyProductAttribute("GetStartedApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GetStartedApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
